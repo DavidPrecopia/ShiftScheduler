bfbb04b355c294782da5d188c68ae7ce02800127
David Precopia <35779726+DavidPrecopia@users.noreply.github.com>
2023-12-23 19:59:32 -0700

    Pre-populating the database

--- data/src/main/kotlin/com/precopia/data/database/ShiftSchedulerDatabase.kt
+++ data/src/main/kotlin/com/precopia/data/database/ShiftSchedulerDatabase.kt
@@ -4,11 +4,13 @@ import android.app.Application
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.precopia.data.constants.DatabaseConstants
 import com.precopia.data.dao.SelectedWeekDao
 import com.precopia.data.dao.WeekDao
 import com.precopia.data.datamodel.SelectedWeekDb
 import com.precopia.data.datamodel.WeekDb
+import java.util.concurrent.Executors
 
 @Database(
     entities = [WeekDb::class, SelectedWeekDb::class],
@@ -20,16 +22,35 @@ internal abstract class ShiftSchedulerDatabase: RoomDatabase() {
     companion object {
         private var database: ShiftSchedulerDatabase? = null
         
-        fun getInstance(application: Application): ShiftSchedulerDatabase {
+        fun getInstance(application: Application): ShiftSchedulerDatabase =
+            database ?: synchronized(this) {
+                database ?: buildDatabase(application).also { database = it }
+            }
+        
+        private fun buildDatabase(application: Application): ShiftSchedulerDatabase {
             if (database === null) {
                 database = Room.databaseBuilder(
                     application,
                     ShiftSchedulerDatabase::class.java,
                     DatabaseConstants.DATABASE_NAME
-                ).build()
+                ).addCallback(object : Callback() {
+                    override fun onCreate(db: SupportSQLiteDatabase) {
+                        super.onCreate(db)
+                        Executors.newSingleThreadExecutor().execute {
+                            getInstance(application)
+                                .weekDao()
+                                .addWeeks(PREPOPULATE_DATA)
+                        }
+                    }
+                }).build()
             }
             return database!!
         }
+        
+        val PREPOPULATE_DATA = listOf(
+            WeekDb(1, "A Week", "072523"),
+            WeekDb(2, "B Week", "080123")
+        )
     }
     
     abstract fun weekDao(): WeekDao
